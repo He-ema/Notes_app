67378124b69c085ae622138b947f6568f639475d
ibrahim Osama <zx.hema72@gmail.com>
2023-07-25 02:20:52 +0300

    add note method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/constants.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/notes_view.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -7,6 +8,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
