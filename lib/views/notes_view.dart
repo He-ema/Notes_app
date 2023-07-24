0382f46edc5d16ff98a9b16892c5d413d177f48d
ibrahim Osama <zx.hema72@gmail.com>
2023-07-24 04:08:45 +0300

    show modal bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/add_note_buttom_sheet.dart';
 import 'package:noteapp/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,7 +9,15 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
+              context: context,
+              builder: (context) {
+                return AddNoteButtomSheet();
+              });
+        },
         child: Icon(Icons.add),
       ),
       body: const NotesViewBody(),
