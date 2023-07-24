254f8e337799f1a0c55383b4fcb84e6afa2b3214
ibrahim Osama <zx.hema72@gmail.com>
2023-07-24 03:47:18 +0300

    adding floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/views/notes_view.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const NotesApp());
 }
 
